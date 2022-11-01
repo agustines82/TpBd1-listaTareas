9b5fb4d52e241108c0570333d36da66dd9c4e1a7
Agustin Sampayo <agustines82@gmail.com>
2022-11-01 17:37:44 -0300

    configuro middlewares, morgan y creo un archivo estatico

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import express from "express";
-console.log("hola mundo");
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
 
+console.log("hola mundo");
+//instancio express
 const app = express();
 
+//CONFIGURACION DEL PUERTO
 app.set("port", process.env.PORT || 4001);
-
+//para usar el puerto
 app.listen(app.get("port"), () => {
     console.log("Estoy en el puerto " + app.get("port"));
 });
+
+//MIDDLEWARES:
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ entends: true }));
+app.use(morgan("dev"));
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname, "../public")));
+
+//RUTAS
